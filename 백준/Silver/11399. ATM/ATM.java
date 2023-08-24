057fe91f885b61f170de3ef1671a1204d5d0f77b
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-08-24 23:40:18 +0900

    [Silver IV] Title: ATM, Time: 144 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		int result = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			count += arr[i];
+			result += count;
+		}
+		System.out.println(result);
+	}
+}
