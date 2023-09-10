d8ad6410fbf280b83504641a1d2f7f53486e5826
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-09-10 21:56:38 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Character> set;
+		int n = Integer.parseInt(br.readLine());	
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			set = new HashSet<>();
+			boolean b = false;
+			for (int j = 0; j < str.length(); j++) {
+				char a = str.charAt(j);
+				
+				if (j == str.length() - 1) {
+					if (set.contains(a))
+						b = true;
+					break;
+				}
+
+				if (a != str.charAt(j + 1)) {
+					if (set.contains(a)) {
+						b = true;
+						break;
+
+					} else {
+						set.add(a);
+					}
+				}
+			}
+			if (!b)
+				count++;
+		}
+		System.out.println(count);
+	}
+}
