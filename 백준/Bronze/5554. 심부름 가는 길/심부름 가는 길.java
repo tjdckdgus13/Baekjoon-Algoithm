52e4893587bd7ccec41dfcbfe60590f08d847703
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-10-06 15:02:22 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0;
+		for (int i = 0; i < 4; i++) {
+			sum += Integer.parseInt(br.readLine());
+		}      
+		StringBuilder sb = new StringBuilder();
+		sb.append(sum / 60).append("\n").append(sum % 60);
+		System.out.println(sb.toString());
+	}
+}
