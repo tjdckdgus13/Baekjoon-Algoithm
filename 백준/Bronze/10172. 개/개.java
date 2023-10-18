6d1030d012a1188b8694a54f7b4cf118f8bf0970
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-10-18 23:30:14 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
