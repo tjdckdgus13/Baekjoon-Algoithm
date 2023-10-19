8d3955da41660e7a0a30770db64e198547de652e
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-10-19 23:34:22 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,6 @@
+import java.io.IOException;
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("강한친구 대한육군 \n강한친구 대한육군");
+	}
+}
