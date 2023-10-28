0983eee096902db3236df2aa8d4f75a583134b8e
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-10-28 23:06:28 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String str;
+		StringTokenizer st;
+		while ((str = br.readLine()) != null) {
+			st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a + b).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
