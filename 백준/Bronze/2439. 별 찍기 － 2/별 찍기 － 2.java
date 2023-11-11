3c85ff0f0d7bad3e092644ff52fb118d652d8b64
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-11-11 22:40:20 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N - i; j++) {
+				sb.append(" ");
+			}
+			for(int k = 0 ; k < i; k++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+}
