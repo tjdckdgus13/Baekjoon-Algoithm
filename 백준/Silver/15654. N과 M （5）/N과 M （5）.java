cde3d19d41ec754bc2af1b04b19c96f83e9cee1e
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2024-02-25 21:59:25 +0900

    [Silver III] Title: N과 M (5), Time: 416 ms, Memory: 41684 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int arr[], result[];
+	static int N, M;
+	static boolean check[];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+		check = new boolean[N];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		make(0, 0);
+
+		System.out.println(sb.toString());
+	}
+
+	public static void make(int length, int idx) {
+		if (length == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(result[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (check[i])
+				continue;
+
+			result[idx] = arr[i];
+			check[i] = true;
+			make(length + 1, idx + 1);
+			check[i] = false;
+		}
+	}
+}
