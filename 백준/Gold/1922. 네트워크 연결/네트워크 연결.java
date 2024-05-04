a9a6fea6d1db71477c0fd6865152d6fc1c634d1d
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2024-05-05 00:05:23 +0900

    [Gold IV] Title: 네트워크 연결, Time: 528 ms, Memory: 46888 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<Node>[] list;
+    static int[] parent;
     static int N, M;
-    static boolean[] visit;
+    static PriorityQueue<Node> queue = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
@@ -12,63 +12,71 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        list = new ArrayList[N + 1];
-        visit = new boolean[N + 1];
-
-        for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
+        parent = new int[N + 1];
+        make();
 
         StringTokenizer st;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            list[a].add(new Node(b, c));
-            list[b].add(new Node(a, c));
+            queue.add(new Node(a, b, c));
         }
 
         System.out.println(bfs());
     }
 
     static int bfs() {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(1, 0));
-
         int sum = 0;
-        int count = 0;
         while (!queue.isEmpty()) {
-            Node curNode = queue.poll();
-            int cur = curNode.end;
-            int w = curNode.weight;
+            Node node = queue.poll();
 
-            if (visit[cur]) continue;
+            int start = node.start;
+            int end = node.end;
+            int w = node.weight;
 
-            visit[cur] = true;
-            sum += w;
-            count++;
-
-            if (count == N) break;
-
-            for (Node node : list[cur]) {
-                if (!visit[node.end]) {
-                    queue.add(node);
-                }
+            if (union(start, end)) {
+                sum += w;
             }
+
         }
 
         return sum;
     }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    static boolean union(int x, int y) {
+        int X = find(x);
+        int Y = find(y);
+
+        if (X == Y) return false;
+        else {
+            parent[Y] = X;
+            return true;
+        }
+    }
+
+    static void make() {
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+    }
+
 }
 
 class Node implements Comparable<Node> {
+    int start;
     int end;
     int weight;
 
-    public Node(int end, int weight) {
+    public Node(int start, int end, int weight) {
+        this.start = start;
         this.end = end;
         this.weight = weight;
     }
