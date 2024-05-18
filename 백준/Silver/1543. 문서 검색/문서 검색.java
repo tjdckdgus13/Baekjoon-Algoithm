328c8dff027fd9e8dce3dd0aa48346d18fca0dc0
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2024-05-18 23:27:42 +0900

    [Silver V] Title: 문서 검색, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		String S2 = br.readLine();
+
+		int size = S.length();
+		int size2 = S2.length();
+		
+		S = S.replace(S2, "");
+		System.out.println((size - S.length()) / size2);
+	}
+
+}
