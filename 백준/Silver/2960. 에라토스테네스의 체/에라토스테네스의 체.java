f9795bbbea455b7ae292658298946b047a524d06
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2024-05-24 00:05:30 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int k = sc.nextInt();
+    	boolean[] check = new boolean[n+1];
+    	
+    	int cnt = 0;
+    	for(int i = 2; i<=n; i++) {
+    		for(int j = i; j<=n; j+=i) {
+    			if(check[j] == false) {
+        			cnt++;
+        			check[j] = true;
+    			}
+
+    			if(cnt == k) {
+    				System.out.println(j);
+    				System.exit(0);
+    			}
+    		}
+    	}
+    }
+}
