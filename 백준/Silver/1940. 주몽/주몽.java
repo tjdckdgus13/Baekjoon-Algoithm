c0780020b9032c53c31ca38ee3c2f1e682cfa7c7
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2024-06-19 23:27:44 +0900

    [Silver IV] Title: 주몽, Time: 164 ms, Memory: 16516 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int j = N - 1;
+        int count = 0;
+        for (int i = 0; i < j; ) {
+            int sum = arr[i] + arr[j];
+            if (sum == M) {
+                count++;
+                j--;
+            } else if (sum > M) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
